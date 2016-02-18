15397a6ac9ab650caa47aed9bc39fbc4cd78c8fc
Reginald Tan <redge.tan@gmail.com>
2016-02-18 14:24:25 -0500

    http download webpage

--- app/src/main/java/com/sandbox/myfirstapp/app/MyActivity.java
+++ app/src/main/java/com/sandbox/myfirstapp/app/MyActivity.java
@@ -1,7 +1,11 @@
 package com.sandbox.myfirstapp.app;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -16,19 +20,24 @@ import android.widget.TextView;
 import android.widget.VideoView;
 import com.sandbox.myfirstapp.app.util.CustomJSONSerializer;
 import org.json.JSONException;
+import org.w3c.dom.Text;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MyActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.sandbox.myfirstapp.MESSAGE";
+    private TextView debugView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        debugView = (TextView) findViewById(R.id.display_debug);
 
         String dataDir = Environment.getDataDirectory().getAbsolutePath();
         String packageDir = getExternalFilesDir(null).getAbsolutePath();
@@ -62,23 +71,24 @@ public class MyActivity extends AppCompatActivity {
     }
 
     public void sendMessage(View view) {
-        String packageDir = getExternalFilesDir(null).getAbsolutePath() + "/names.json";
-        ArrayList<String> names = new ArrayList<String>();
-        names.add("roger");
-        names.add("dasani");
-        try {
-            new CustomJSONSerializer(getApplicationContext(),packageDir).save(names);
-        } catch (JSONException e) {
-            Log.e("MARIO",Log.getStackTraceString(e));
-        } catch (IOException e) {
-            Log.e("MARIO",Log.getStackTraceString(e));
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // fetch data
+            String stringUrl = "http://www.indeed.ca";
+            new DownloadWebpageTask().execute(stringUrl);
+        } else {
+            // display error
+            debugView.setText(R.string.no_network_connection);
+            return;
         }
 
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+//        Intent intent = new Intent(this, DisplayMessageActivity.class);
+//        EditText editText = (EditText) findViewById(R.id.edit_message);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
     }
 
     public void playLocalVideo(View view) {
@@ -87,5 +97,63 @@ public class MyActivity extends AppCompatActivity {
         mVideoView.setMediaController(new MediaController(this));
         mVideoView.requestFocus();
         mVideoView.start();
+    }
+
+    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            debugView.setText(result);
+        }
+    }
+
+    private String downloadUrl(String myurl) throws IOException {
+        InputStream is = null;
+        // Only display the first 500 characters of the retrieved
+        // web page content.
+        int len = 500;
+
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d("MARIO", "The response is: " + response);
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            String contentAsString = readIt(is, len);
+            return contentAsString;
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+    public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+        Reader reader = null;
+        reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[len];
+        reader.read(buffer);
+        return new String(buffer);
     }
 }
