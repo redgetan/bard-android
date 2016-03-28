a5528090127e639632559406cfd6bd716ed88d37
Reginald Tan <redge.tan@gmail.com>
2016-03-27 23:20:59 -0400

    initial google analytics + crashlytics setup

--- app/src/main/java/com/sandbox/myfirstapp/app/ClientApp.java
+++ app/src/main/java/com/sandbox/myfirstapp/app/ClientApp.java
@@ -1,16 +1,36 @@
 package com.sandbox.myfirstapp.app;
 
 import android.app.Application;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 
 
 public class ClientApp extends Application {
+    private Tracker mTracker;
+
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         RealmConfiguration config = new RealmConfiguration.Builder(this).build();
         Realm.setDefaultConfiguration(config);
+    }
+
+    /**
+     * Gets the default {@link Tracker} for this {@link Application}.
+     * @return tracker
+     */
+    synchronized public Tracker getDefaultTracker() {
+        if (mTracker == null) {
+            GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
+            // To enable debug logging use: adb shell setprop log.tag.GAv4 DEBUG
+            mTracker = analytics.newTracker(R.xml.global_tracker);
+        }
+        return mTracker;
     }
 
     @Override
