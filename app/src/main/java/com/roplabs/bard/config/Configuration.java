78012fd23f2c7d68d9ea81902756947218f89581
Reginald Tan <redge.tan@gmail.com>
2016-09-09 13:08:47 -0400

    bump version

--- app/src/main/java/com/roplabs/bard/config/Configuration.java
+++ app/src/main/java/com/roplabs/bard/config/Configuration.java
@@ -5,7 +5,7 @@ import com.roplabs.bard.BuildConfig;
 public class Configuration {
     public static String bardAPIBaseURL() {
         if (BuildConfig.BUILD_TYPE.equals("debug")) {
-            return "http://10.88.111.14:3000";
+            return "https://bard.co";
         } else {
             return "https://bard.co";
         }
