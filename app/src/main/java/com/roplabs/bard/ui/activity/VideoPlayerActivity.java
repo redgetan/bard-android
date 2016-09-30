f899376b69ba5ca0b78c19cec9b8fb047f086b36
Reginald Tan <redge.tan@gmail.com>
2016-09-30 18:19:17 -0400

    repo view should be replayable on click

--- app/src/main/java/com/roplabs/bard/ui/activity/VideoPlayerActivity.java
+++ app/src/main/java/com/roplabs/bard/ui/activity/VideoPlayerActivity.java
@@ -4,16 +4,16 @@ import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.widget.PopupMenu;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.MediaController;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.VideoView;
+import android.widget.*;
 import com.instabug.library.Instabug;
 import com.roplabs.bard.ClientApp;
 import com.roplabs.bard.R;
@@ -31,6 +31,9 @@ public class VideoPlayerActivity extends BaseActivity implements PopupMenu.OnMen
     private String videoLocation;
     private String repoToken;
     private String repoUrl;
+    private VideoView videoView;
+    private MediaPlayer mediaPlayer;
+    private boolean isVideoReady = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,14 +47,59 @@ public class VideoPlayerActivity extends BaseActivity implements PopupMenu.OnMen
         String repoTitle = intent.getStringExtra("title");
 
         TextView title = (TextView) toolbar.findViewById(R.id.toolbar_title);
+
         title.setText(repoTitle);
 
-        VideoView videoView = (VideoView) findViewById(R.id.video_view);
-        videoView.setMediaController(new MediaController(this));
-        videoView.setVideoPath(this.videoLocation);
-        videoView.requestFocus();
-        videoView.start();
+        videoView = (VideoView) findViewById(R.id.video_view);
+        initVideoPlayer();
+        playLocalVideo(this.videoLocation);
 
+    }
+
+    private void playLocalVideo(String filePath) {
+        videoView.setVideoPath(filePath);
+        videoView.start();
+    }
+
+
+    private void initVideoPlayer() {
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+            }
+        });
+
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                videoView.setBackgroundColor(Color.TRANSPARENT);
+                isVideoReady = true;
+                mediaPlayer = mp;
+            }
+        });
+
+        videoView.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // http://stackoverflow.com/a/14163267
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    if (!isVideoReady) return false;
+
+                    replayVideo();
+
+                    return false;
+                } else {
+                    return true;
+                }
+
+            }
+        });
+    }
+
+    public void replayVideo() {
+        mediaPlayer.seekTo(0);
+        mediaPlayer.start();
     }
 
     @Override
