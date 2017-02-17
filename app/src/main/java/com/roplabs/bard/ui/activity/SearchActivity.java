8e8e869fa2b9b287ae91067f75c943035c28ac88
Reginald Tan <redge.tan@gmail.com>
2017-02-17 18:04:05 -0500

    separate search page

--- app/src/main/java/com/roplabs/bard/ui/activity/SearchActivity.java
+++ app/src/main/java/com/roplabs/bard/ui/activity/SearchActivity.java
@@ -1,13 +1,10 @@
 package com.roplabs.bard.ui.activity;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.*;
@@ -17,46 +14,36 @@ import android.widget.*;
 import com.roplabs.bard.R;
 import com.roplabs.bard.adapters.SceneListAdapter;
 import com.roplabs.bard.api.BardClient;
-import com.roplabs.bard.models.Character;
 import com.roplabs.bard.models.Scene;
 import com.roplabs.bard.ui.widget.ItemOffsetDecoration;
 import com.roplabs.bard.util.*;
 import io.realm.Realm;
-import io.realm.RealmChangeListener;
-import io.realm.RealmResults;
-import org.greenrobot.eventbus.EventBus;
 import org.json.JSONException;
 import org.json.JSONObject;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-import java.io.IOException;
+import android.support.v7.widget.SearchView;
+
+
 import java.util.*;
 
-import static com.roplabs.bard.util.Helper.LOGIN_REQUEST_CODE;
-import static com.roplabs.bard.util.Helper.REQUEST_WRITE_STORAGE;
-
-public class SceneSelectActivity extends BaseActivity  {
+public class SearchActivity extends BaseActivity {
     private Context mContext;
-    private DrawerLayout mDrawerLayout;
     private RecyclerView recyclerView;
     private ProgressBar progressBar;
     public List<Scene> sceneList;
     private EndlessRecyclerViewScrollListener scrollListener;
-    private EditText searchBar;
-    private TextView videoListLabel;
-    private TextView pickVideoLabel;
 
     private final static int LEFT_DRAWABLE_INDEX = 0;
     private final static int RIGHT_DRAWABLE_INDEX = 2;
     private final static int BARD_EDITOR_REQUEST_CODE = 1;
     private String lastSearch;
-    private Drawable searchIcon;
-    private Drawable clearIcon;
     private FrameLayout emptyStateContainer;
     private TextView emptyStateTitle;
     private TextView emptyStateDescription;
+    private SearchView searchView;
 
     private HashMap<String, List<Scene>> sceneListCache;
     private int sceneListCacheExpiry;
@@ -67,45 +54,24 @@ public class SceneSelectActivity extends BaseActivity  {
         BardLogger.log("Scene Select onCreate");
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scene_select);
+        setContentView(R.layout.activity_search);
 
         mContext = this;
         recyclerView = (RecyclerView) findViewById(R.id.scene_list);
         progressBar = (ProgressBar) findViewById(R.id.scene_progress_bar);
-        searchBar = (EditText) findViewById(R.id.video_search_input);
-        videoListLabel = (TextView) findViewById(R.id.video_list_label);
-        pickVideoLabel = (TextView) findViewById(R.id.pick_video_label);
-        searchIcon = searchBar.getCompoundDrawables()[LEFT_DRAWABLE_INDEX];
-        clearIcon = searchBar.getCompoundDrawables()[RIGHT_DRAWABLE_INDEX];
-
         sceneListCache = new HashMap<String, List<Scene>>();
         sceneListCacheExpiry = Calendar.getInstance().get(Calendar.SECOND) + (60 * 60); // 1 hour
 
-        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        this.getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_clear_white_24dp);
         TextView title = (TextView) toolbar.findViewById(R.id.toolbar_title);
-        title.setText(R.string.app_name);
-        title.setTextSize(24);
+        title.setText(R.string.search);
 
-        deepLinkNavigate();
-
-        Helper.initNavigationViewDrawer(this, toolbar);
         initEmptyState();
         initSearch();
         initScenes();
-        Helper.askStoragePermission(this);
-    }
 
-    private void deepLinkNavigate() {
-        Intent intent = getIntent();
-        String sceneToken;
-
-        if ((sceneToken = intent.getStringExtra("sceneTokenEditorDeepLink")) != null) {
-            Intent newIntent = new Intent(this, BardEditorActivity.class);
-            newIntent.putExtra("sceneToken", sceneToken);
-            BardLogger.trace("[sceneSelect] " + sceneToken);
-            startActivityForResult(newIntent, BARD_EDITOR_REQUEST_CODE);
-        }
+        // show keyboard
+//        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+//        imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT,0);
     }
 
     private void initEmptyState() {
@@ -117,54 +83,49 @@ public class SceneSelectActivity extends BaseActivity  {
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case REQUEST_WRITE_STORAGE: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_search_field, menu);
 
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-
-                } else {
-                    Toast.makeText(this, "The app was not allowed to write to your storage. Hence, it cannot function properly. Please consider granting it this permission", Toast.LENGTH_LONG).show();
-
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                return;
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        // Expand the search view and request focus
+        searchItem.expandActionView();
+        searchView.requestFocus();
+        MenuItemCompat.setOnActionExpandListener(searchItem, new MenuItemCompat.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+                return false;
             }
 
-            // other 'case' lines to check for other
-            // permissions this app might request
-        }
-    }
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+                finish();
+                return false;
+            }
+        });
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_search, menu);
-//        getMenuInflater().inflate(R.menu.menu_settings, menu);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // perform query here
+                performSearch(query);
+
+                // workaround to avoid issues with some emulators and keyboard devices firing twice if a keyboard enter is used
+                // see https://code.google.com/p/android/issues/detail?id=24599
+                searchView.clearFocus();
+
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+
+
         return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        Intent intent;
-        switch (item.getItemId()) {
-            case R.id.menu_item_settings:
-                intent = new Intent(this, ProfileActivity.class);
-                startActivity(intent);
-                return true;
-            case R.id.menu_item_search:
-                intent = new Intent(this, SearchActivity.class);
-                startActivity(intent);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
     }
 
     private void displayInitialSearchMessage() {
@@ -185,56 +146,6 @@ public class SceneSelectActivity extends BaseActivity  {
 
     private void initSearch() {
         lastSearch = "";
-        searchIcon.setAlpha(100); // make search icon opacity set to 50%
-        clearIcon.setAlpha(0); // invisible at beginning
-
-        searchBar.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                    BardLogger.log("user clicked search icon ...");
-                    performSearch(v.getText().toString());
-                    return true;
-                }
-                return false;
-            }
-        });
-
-        searchBar.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if (event.getAction()==KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
-                    performSearch(searchBar.getText().toString());
-                    return true;
-                }
-                if (!searchBar.getText().toString().isEmpty()) {
-                    clearIcon.setAlpha(100); // make it visible if there's text
-                } else {
-                    clearIcon.setAlpha(0); // hide if empty
-                }
-                return false;
-            }
-        });
-
-        searchBar.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                final int DRAWABLE_LEFT = 0;
-                final int DRAWABLE_TOP = 1;
-                final int DRAWABLE_RIGHT = 2;
-                final int DRAWABLE_BOTTOM = 3;
-
-                if(event.getAction() == MotionEvent.ACTION_UP) {
-                    if(event.getRawX() >= (searchBar.getRight() - clearIcon.getBounds().width())) {
-                        // clear button clicked
-                        searchBar.setText("");
-                        clearIcon.setAlpha(0); // hide clear button again
-                        return true;
-                    }
-                }
-                return false;
-            }
-        });
     }
 
     private void performSearch(String text) {
@@ -330,21 +241,13 @@ public class SceneSelectActivity extends BaseActivity  {
 
     @Override
     protected void onResume() {
-        BardLogger.log("Scene Select onResume");
+        BardLogger.log("Search onResume");
 
         int timeNow = Calendar.getInstance().get(Calendar.SECOND);
         if (timeNow > sceneListCacheExpiry) {
             sceneListCache.clear();
         }
 
-        // fetch data if blank (i.e. previously no internet connection)
-        if (recyclerView.getAdapter().getItemCount() == 0) {
-            Map<String, String> map = new HashMap<String, String>();
-            map.put("page",String.valueOf(1));
-            syncRemoteData(map);
-        }
-
-        Helper.initNavigationViewDrawer(this, toolbar);
         super.onResume();
     }
 
@@ -364,10 +267,6 @@ public class SceneSelectActivity extends BaseActivity  {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == RESULT_OK && requestCode == BARD_EDITOR_REQUEST_CODE) {
-            finish();
-        } else if (resultCode == RESULT_OK && requestCode == LOGIN_REQUEST_CODE) {
-        }
     }
 
     private void populateScenes(List<Scene> remoteSceneList, Map<String, String> options) {
@@ -401,7 +300,7 @@ public class SceneSelectActivity extends BaseActivity  {
             public void onItemClick(View itemView, int position, Scene scene) {
                 Intent intent = new Intent(self, BardEditorActivity.class);
                 intent.putExtra("sceneToken", scene.getToken());
-                BardLogger.trace("[sceneSelect] " + scene.getToken());
+                BardLogger.trace("[sceneSearch] " + scene.getToken());
                 startActivityForResult(intent, BARD_EDITOR_REQUEST_CODE);
             }
         });
@@ -432,7 +331,7 @@ public class SceneSelectActivity extends BaseActivity  {
         Map<String, String> data = new HashMap<String, String>();
         data.put("page", String.valueOf(page));
 
-        String search = searchBar.getText().toString();
+        String search = searchView.getQuery().toString();
 
         if (!search.isEmpty()) {
             data.put("search", search);
